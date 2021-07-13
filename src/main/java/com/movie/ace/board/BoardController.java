cfba93050345c20c6ebb200c9249461c5a6fd025
omy414 <omy414@syworks.com>
2021-07-13 18:15:35 +0900

    머지테스트5

--- src/main/java/com/movie/ace/board/BoardController.java
+++ src/main/java/com/movie/ace/board/BoardController.java
@@ -66,6 +66,7 @@ public class BoardController {
 			System.out.println("test4");
 			System.out.println("test5");
 			System.out.println("test6");
+			System.out.println("test7");
 		System.out.println(keyword);
 
 		ModelAndView mav = new ModelAndView();
